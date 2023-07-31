10e2f7e1ff695e21e009f8555b6f1d8cc274dbd2
aaaa777 <aaaa777@users.noreply.github.com>
2023-08-01 08:45:48 +0900

    rollback

--- lib/core/version.py
+++ lib/core/version.py
@@ -2,7 +2,7 @@ from .utils import Version
 
 class AppVersion(Version):
     version_major = 0
-    version_minor = 2
+    version_minor = 1
     version_patch = 0
 
     singleton = Version(version_major, version_minor, version_patch)
