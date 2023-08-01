a3b8af065f1c86a5b17fa336dcaccf576a59d57d
aaaa777 <aaaa777@users.noreply.github.com>
2023-08-01 10:19:13 +0900

    version up v0.1.2

--- lib/core/version.py
+++ lib/core/version.py
@@ -3,6 +3,6 @@ from .utils import Version
 class AppVersion(Version):
     version_major = 0
     version_minor = 1
-    version_patch = 1
+    version_patch = 2
 
     version = Version(version_major, version_minor, version_patch)
